9ea464a6a8d87f4576022f69c60c4bb75b3ea4b9
HasithDeAlwis <hasithde24@gmail.com>
2023-11-14 12:04:28 -0500

    added spotify route to add playlist

--- backend/controllers/spotify/spotifyController.py
+++ backend/controllers/spotify/spotifyController.py
@@ -1,7 +1,8 @@
 from .spotifyKeyInfo import API_INFORMATION, AUTH_URL, TOKEN_URL, API_BASE_URL
 import urllib.parse
-from flask import request, jsonify
+from flask import request, jsonify, redirect, session, url_for
 import requests
+from datetime import datetime
 
 #returns the url that will be called to start the OAuth Code Flow
 def getSpotifyAuthURL() -> str:
@@ -44,3 +45,40 @@ def getToken():
         token_info = response.json()
         #return it
         return token_info
+
+#when the token expires refresh the token
+def refreshToken() -> jsonify:
+    #if the token is not in session then get the user to authenticate it
+    if 'refresh_token' not in session: 
+        return redirect('/api/spotify/authenticate')
+    #check to see if the token is acc expired
+    if (datetime.now().timeStamp > session['expires_at']):
+        #create new body to refresh the token
+        req_body = {
+            'grant_type': 'refresh_token',
+            'refresh_token': session['refresh_token'],
+            'client_id': API_INFORMATION[0],
+            'client_secret': API_INFORMATION[1]
+        }
+
+    #get the toekn info
+    response = requests.get(TOKEN_URL, data=req_body)
+    #use json to convert token data
+    new_token_info = response.json()
+    return new_token_info
+
+
+def getPlaylists():
+    if 'access_token' not in session:
+        return redirect('/login')
+    
+    if session['expires_at'] < datetime.now().timestamp():
+        return redirect('/refresh-token')
+    headers = {
+        'Authorization': f"Bearer {session['access_token']}",
+    }
+    
+    response = requests.get(API_BASE_URL + 'me/playlists', headers=headers)
+    
+    playlist = response.json()
+    return jsonify(playlist)
--- backend/routes/spotifyRoutes.py
+++ backend/routes/spotifyRoutes.py
@@ -1,6 +1,6 @@
 #this file will contain all the API routes that will make calls to the spotify API
-from flask import Blueprint, redirect, session, request, jsonify
-from ..controllers.spotify.spotifyController import getSpotifyAuthURL, getToken
+from flask import Blueprint, redirect, session, request, jsonify, Response
+from ..controllers.spotify.spotifyController import getSpotifyAuthURL, getToken, refreshToken, getPlaylists
 from datetime import datetime
 import requests 
 
@@ -10,9 +10,12 @@ spotify = Blueprint('spotify', __name__)
 #authenticate will be used to intially make the call to the spotify API to start OAtuh2.0 code flow
 @spotify.route('/authenticate')
 def authenticate():
+    #get the authorization url that spoitfy needs 
     auth_url = getSpotifyAuthURL()
+    #go to the auth_url which calls our callback route
     return redirect(auth_url)
 
+#call back uri for the spotify API to call to atuhetnicate the API key
 @spotify.route("/callback")
 def setTokenInfo():
     #get the token
@@ -22,3 +25,30 @@ def setTokenInfo():
     session['access_token'] = token_info['access_token'] 
     session['refresh_token'] = token_info['refresh_token']
     session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
+    return redirect
+
+#route to refresh token once it has expired
+@spotify.route('/refresh-token')
+def refresh_token():
+    #get the new token 
+    new_token_info = refreshToken()
+    #change the session info
+    session['access_token'] = new_token_info['access_token']
+    session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
+    
+@spotify.route('/top-songs')
+def getTopSongs():
+    pass
+
+@spotify.route('/recently-played')
+def getRecentlyPlayed():
+    pass
+
+
+@spotify.route('/top-artists')
+def getTopArtist():
+    pass
+
+@app.route('/playlist')
+def get_playlist():
+    playlistJSON = getPlaylists()
